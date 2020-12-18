ad73fbc733f921a7be79c72184924ea5a9cd5b36
syJSdev <whitedragon226@gmail.com>
2020-12-18 12:40:11 -0600

    update the rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import babel from 'rollup-plugin-babel'
 import autoExternal from 'rollup-plugin-auto-external'
+import includePaths from 'rollup-plugin-includepaths'
 
 export default {
   input: 'src/index.js',
@@ -14,6 +15,12 @@ export default {
     }
   ],
   plugins: [
+    includePaths({
+      include: {},
+      paths: ['src'],
+      external: [],
+      extensions: ['.js']
+  }),
     babel({
       presets: [['@babel/preset-env', { targets: { node: '8.3' } }]],
       comments: false
